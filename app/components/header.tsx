a9c5a877de5af8294fe78b5695c612ce21191d91
董佳华 <2595857151@qq.com>
2024-03-27 18:56:45 +0800

    去掉 AppIcon

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -4,7 +4,7 @@ import {
   Bars3Icon,
   PencilSquareIcon,
 } from '@heroicons/react/24/solid'
-import AppIcon from '@/app/components/base/app-icon'
+// import AppIcon from '@/app/components/base/app-icon'
 export type IHeaderProps = {
   title: string
   isMobile?: boolean
@@ -30,8 +30,8 @@ const Header: FC<IHeaderProps> = ({
         )
         : <div></div>}
       <div className='flex items-center space-x-2'>
-        <AppIcon size="small" />
-        <div className=" text-sm text-gray-800 font-bold">{title}</div>
+        {/* <AppIcon size="small" /> */}
+        {/* <div className=" text-sm text-gray-800 font-bold">{title}</div> */}
       </div>
       {isMobile
         ? (
